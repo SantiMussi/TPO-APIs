aa8c327e4c0ae5bcb00fc706d9a8b6689242eca5
Marcos Villar <marcos.villar.mail@gmail.com>
2025-09-09 17:40:05 -0300

    + Encriptacion de contraseñas cuando se cambian - OrderRepository.Java

--- src/main/java/com/uade/tpo/demo/controllers/user/UserController.java
+++ src/main/java/com/uade/tpo/demo/controllers/user/UserController.java
@@ -1,5 +1,7 @@
 package com.uade.tpo.demo.controllers.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,14 +19,17 @@ import com.uade.tpo.demo.controllers.user.UserChangeRequest;
 
 @RestController
 @RequestMapping("users")
+@RequiredArgsConstructor
 public class UserController {
     @Autowired
     private UserService userService;
 
+    private final PasswordEncoder passwordEncoder;
+
     @PutMapping("/{id}")
     public ResponseEntity<Object> changeUserInfo(@PathVariable Long id, @RequestBody UserChangeRequest request){
         try{
-            User updatedUser = userService.changeUserInfo(id, request.getEmail(), request.getName(), request.getPassword(), request.getFirstName(), request.getLastName());
+            User updatedUser = userService.changeUserInfo(id, request.getEmail(), request.getName(), passwordEncoder.encode(request.getPassword()), request.getFirstName(), request.getLastName());
             return ResponseEntity.ok(updatedUser);
         } catch (UserDuplicateException e) {
             return ResponseEntity.status(409).body("Email already exists.");
